425effae8a78ef73f30eb8cdc018fde76098a620
Stefan Niederhauser <stefan.niederhauser@mimacom.com>
2014-05-08 23:24:04 +0200

    update dependency

--- src/main/java/guru/nidi/ramltester/uc/servlet/RamlFilter.java
+++ src/main/java/guru/nidi/ramltester/uc/servlet/RamlFilter.java
@@ -2,7 +2,7 @@ package guru.nidi.ramltester.uc.servlet;
 
 import guru.nidi.ramltester.RamlDefinition;
 import guru.nidi.ramltester.RamlViolations;
-import guru.nidi.ramltester.servlet.RamlTesters;
+import guru.nidi.ramltester.TestRaml;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,12 +19,12 @@ public class RamlFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        api = RamlDefinition.fromClasspath(getClass(), "api.yaml");
+        api = TestRaml.load("api.yaml").fromClasspath(getClass());
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        final RamlViolations violations = RamlTesters.executeFilterChain(api, request, response, chain);
+        final RamlViolations violations = api.testAgainst(request, response, chain);
         log.info("Violations: " + violations);
     }
 
