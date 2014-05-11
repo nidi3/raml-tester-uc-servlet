85dcac8bf05425b85b3c79a55bbbfae48c0b8f54
Stefan Niederhauser <stefan.niederhauser@mimacom.com>
2014-05-12 00:40:04 +0200

    update dependency, license

--- src/main/java/guru/nidi/ramltester/uc/servlet/RamlFilter.java
+++ src/main/java/guru/nidi/ramltester/uc/servlet/RamlFilter.java
@@ -1,8 +1,23 @@
+/*
+ * Copyright (C) ${project.inceptionYear} Stefan Niederhauser (nidin@gmx.ch)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package guru.nidi.ramltester.uc.servlet;
 
 import guru.nidi.ramltester.RamlDefinition;
-import guru.nidi.ramltester.RamlViolations;
 import guru.nidi.ramltester.TestRaml;
+import guru.nidi.ramltester.core.RamlReport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,8 +39,8 @@ public class RamlFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        final RamlViolations violations = api.testAgainst(request, response, chain);
-        log.info("Violations: " + violations);
+        final RamlReport report = api.testAgainst(request, response, chain);
+        log.info("Raml report: " + report);
     }
 
     @Override
